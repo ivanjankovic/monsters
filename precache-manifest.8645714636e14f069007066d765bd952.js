38d8b984bf9f271f299ebd89ec9977b6351b4410
ivan Jankovic <jankoemail@gmail.com>
2020-08-26 09:52:10 -0400

    Updates

---
+++ precache-manifest.8645714636e14f069007066d765bd952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79882f0776247aeff5cd8f129da49733",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "cc0cd80039c3ccdcc905",
+    "url": "/monsters/static/css/main.3526471f.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0cd80039c3ccdcc905",
+    "url": "/monsters/static/js/main.bf6b781c.chunk.js"
+  },
+  {
+    "revision": "92788b254d42c8cdca2a",
+    "url": "/monsters/static/js/runtime-main.a7930c39.js"
+  }
+]);
